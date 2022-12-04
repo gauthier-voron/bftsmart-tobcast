13fa78c33f0d4ea3adfbf9d6e6205bf1d549dad9
Gauthier Voron <gauthier.voron@epfl.ch>
2022-12-04 16:53:06 +0100

    raw client report

--- src/main/java/bftsmart/demo/benchmark/Client.java
+++ src/main/java/bftsmart/demo/benchmark/Client.java
@@ -18,19 +18,6 @@ import java.util.logging.Level;
 
 
 public class Client {
-	private static final long[] latencies = new long[] {
-		100, 500, 1000, 5000, 10000, 30000, Long.MAX_VALUE
-	};
-	private static final int[] latencyCounts = new int[latencies.length];
-
-	private static long latencySum = 0;
-
-	private static long latencyCount = 0;
-
-	private static boolean firstReport = true;
-
-	private static long lastReport;
-
 	private static long start;
 
 	private static byte[] tx;
@@ -65,61 +52,8 @@ public class Client {
 			ackTime = System.currentTimeMillis();
 			lat = ackTime - submitTime;
 
-			latencySum += lat;
-			latencyCount += 1;
-
-			for (i = 0; i < latencies.length; i++)
-				if (lat < latencies[i])
-					break;
-
-			if (i >= latencyCounts.length) {
-				System.err.println("ERROR: impossible latency " + lat);
-				return;
-			}
-
-			latencyCounts[i] += 1;
+			System.out.println(submitTime + " -> " + ackTime + " = " + lat);
 		}
-	}
-
-	private static void report() {
-		long now = System.currentTimeMillis();
-		int i, avglat;
-
-		if (now < (lastReport + 1000))
-			return;
-
-		if (firstReport) {
-			System.out.printf("%13s %9s %13s %9s %9s", "time", "#tx", "#B",
-							  "ack", "avg lat");
-			for (i = 0; i < latencies.length; i++) {
-				if (latencies[i] == Long.MAX_VALUE)
-					System.out.printf("     < inf");
-				else
-					System.out.printf(" %9s", "< " + latencies[i] + "ms");
-			}
-			System.out.printf("\n");
-			System.out.printf("------------- --------- ------------- " +
-							  "--------- ---------");
-			for (i = 0; i < latencies.length; i++)
-				System.out.printf(" ---------");
-			System.out.printf("\n");
-
-			firstReport = false;
-		}
-
-		if (latencyCount > 0)
-			avglat = (int) (latencySum / latencyCount);
-		else
-			avglat = 0;
-
-		System.out.printf("%13d %9d %13d %9d %9s", (now - start), done,
-						  done * tx.length, latencyCount, avglat);
-
-		for (i = 0; i < latencyCounts.length; i++)
-			System.out.printf(" %9d", latencyCounts[i]);
-		System.out.printf("\n");
-
-		lastReport = now;
 	}
 
     public static void main(String[] args) throws IOException {
@@ -167,8 +101,6 @@ public class Client {
 				proxy.invokeAsynchRequest
 					(tx, listener, TOMMessageType.ORDERED_REQUEST);
 				done += 1;
-
-				report();
 			}
 
         } catch(Exception e){
